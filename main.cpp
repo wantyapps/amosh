36a5e4ab20c72563d81a344206c5f6ab94916c7f
Uri Arev <uri.arev@gmail.com>
2021-02-21 12:02:46 +0200

    Fixing

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
-#include "funcs.h"
+#include "funcs.hpp"
 #include <readline/readline.h>
 #include <readline/history.h>
 #include "game.hpp"
